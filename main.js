b6d4c1f1eb1088c4ed407bb9a1f938c18db7e3b1
HartMason <hartmason85@gmail.com>
2021-12-08 16:08:17 -0600

    last commit

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTIONS!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
 
-
+const num2StringInput = document.getElementById('num2String')
+const num2StringButton = document.getElementById('num2StringButton')
 // ***************************
 //          PART ONE
 // ***************************
 // Write a JavaScript program to display the current day and time, start with:
-console.log(new Date)
+// console.log(new Date)
 
 const displayDate = () => {
   const currentDate = new Date()
@@ -13,14 +14,29 @@ const displayDate = () => {
   document.getElementById("display-element").innerHTML = currentDate;
 }
  
-
+// num2StringInput.addEventListener
 // Write a JavaScript program to convert a number to a string.
+function num2String() {
+  const number = document.getElementById("quantity").value
+  console.log(number)
 
+  document.getElementById("display-string").innerHTML = '"' + number + '"'
+  
+} // funciton declaration 
+
+// console.log(num2String(42))
 
 
 // Write a JavaScript program to convert a string to the number.
+const str2Num = () => {
+  // return parseInt()
+  const string = document.getElementById("number").value
+  console.log(parseInt(string))
 
+  document.getElementById("display-number").innerHTML = string
+}
 
+console.log(str2Num("108"))
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
@@ -30,17 +46,52 @@ const displayDate = () => {
   // * NaN
   // * String
   
-
-  
+function multipleDatatype(item){
+  if(typeof item === 'boolean'){
+      return 'boolean'; 
+  } else if(item === null){
+      return 'null'; 
+  } else if(item === undefined){
+      return 'undefined'; 
+  } else if(typeof item === 'number'){
+      return 'number';
+  } else if(typeof item === 'string'){
+      return 'string';
+  } else if(isNaN(item)){
+      return 'NaN'; 
+}
+}
 // Write a JavaScript program that adds 2 numbers together.
 
+let num1 = 20
+let num2 = 15
+
+const add = (num1, num2) => {
+  return num1 + num2
+} 
+console.log(add(num1, num2))
 
 
 // Write a JavaScript program that runs only when 2 things are true.
+function bothTrue(name, age) {
+if(name && age > 33) {
+  return true
+}
+  else { 
+    return false;
+  }
+}
 
-
-
+console.log(bothTrue('Matt', 34));
 // Write a JavaScript program that runs when 1 of 2 things are true.
+
+const runWhenOneTrue = (num1, num2) => {
+  if(num1 === true  num2 === false) {
+    return 'true';
+  }
+}
+
+
 
 
 
